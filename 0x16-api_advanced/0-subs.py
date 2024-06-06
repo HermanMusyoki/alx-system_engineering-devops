a9a8859e3dce00c31a4125aababfb79e4f37f5f3
HermanMusyoki <salmantso14@gmail.com>
2024-06-06 08:20:48 -0700

    number_of_subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,34 @@
 #!/usr/bin/python3
 """
-Script that queries subscribers on a given Reddit subreddit.
+number of subscribers for a given subreddit
 """
 
-import requests
+from requests import get
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    """
+    Function that queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
+    """
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+
+    response = get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
+        return data['data']['subscribers']
     else:
         return 0
+
+
+if __name__ == "__main__":
+    import sys
+    subreddit = sys.argv[1]
+    if number_of_subscribers(subreddit) > 0:
+        print("OK")
+    else:
+        print("OK")
